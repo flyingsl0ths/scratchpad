d138b05044d9b86ab77e9da5e39ecd21ff600faf
flyingsl0ths <randomemail32110@gmail.com>
2022-09-11 22:35:46 -0500

    Implement Screenshot component

---
+++ src/components/Screenshot.js
@@ -0,0 +1,88 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import ScreenshotMonitor from "@mui/icons-material/ScreenshotMonitor";
+import TextField from "@mui/material/TextField";
+import * as htmlToImage from "html-to-image";
+import download from "downloadjs";
+
+import { VBox, HBox } from "./Containers";
+import Spacer from "./Spacer";
+import Selection from "./Selection";
+
+export default class Screenshot extends React.Component {
+  static FILE_EXTENSIONS = ["jpeg", "png", "svg"];
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      containsNoFileName: false,
+      fileExtension: "jpeg"
+    };
+  }
+
+  handleScreenshot = () => {
+    const fileName = document.getElementById("screenshot-fn").value;
+
+    if (fileName === "") {
+      this.setState({ containsNoFileName: true });
+      return;
+    }
+
+    const onDownload = dataUrl => {
+      download(dataUrl, `${fileName}.${this.state.fileExtension}`);
+      this.setState({ containsNoFileName: false });
+    };
+
+    const downloadMethod = capitalize(this.state.fileExtension);
+    htmlToImage[`to${downloadMethod}`](
+      document.getElementById("code-window-bg")
+    ).then(onDownload);
+  };
+
+  handleFileExtensionChanged = (_, extension) => {
+    this.setState({ fileExtension: extension });
+  };
+
+  render() {
+    const textfield = this.state.containsNoFileName ? (
+      <TextField
+        error
+        id="screenshot-fn"
+        label="File name"
+        variant="outlined"
+      />
+    ) : (
+      <TextField id="screenshot-fn" label="File name" variant="outlined" />
+    );
+
+    return (
+      <VBox centered={false}>
+        <HBox centered={false}>
+          {textfield}
+
+          <Spacer amount="0.5em" />
+
+          <Selection
+            defaultValue={2}
+            label="File extension"
+            values={Screenshot.FILE_EXTENSIONS}
+            onSelectionChanged={this.handleFileExtensionChanged}
+          />
+        </HBox>
+
+        <Spacer amount="1em" />
+
+        <Button
+          variant="contained"
+          onClick={this.handleScreenshot}
+          startIcon={<ScreenshotMonitor />}>
+          Screenshot
+        </Button>
+      </VBox>
+    );
+  }
+}
+
+function capitalize(fileExtension) {
+  return fileExtension[0].toUpperCase() + fileExtension.slice(1);
+}
