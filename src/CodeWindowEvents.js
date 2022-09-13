ac2acaf9893ff93478c91251cebf4352f4debbe6
flyingsl0ths <randomemail32110@gmail.com>
2022-09-12 23:51:56 -0500

    Replace hard coded values with call to `reduce`

--- src/CodeWindowEvents.js
+++ src/CodeWindowEvents.js
@@ -1,20 +1,26 @@
 const M = {
-  CODE_WINDOW_CHANGES: Object.freeze({
-    LANGUAGE: 1,
-    THEME: 2,
-    VERTICAL_PADDING: 3,
-    HORIZONTAL_PADDING: 4,
-    SHADOW_OFFSET_X: 5,
-    SHADOW_OFFSET_Y: 6,
-    SHADOW_ALPHA: 7,
-    SHADOW_TOGGLED: 8,
-    TITLEBAR: 9,
-    EDITOR_LINES_INCREASED: 10,
-    EDITOR_LINES_TOGGLED: 11,
-    EDITOR_FONT_SIZE_INCREASED: 12,
-    EDITOR_FONT_CHANGED: 13,
-    BG_COLOR: 14
-  }),
+  CODE_WINDOW_CHANGES: Object.freeze(
+    [
+      "LANGUAGE",
+      "THEME",
+      "VERTICAL_PADDING",
+      "HORIZONTAL_PADDING",
+      "SHADOW_OFFSET_X",
+      "SHADOW_OFFSET_Y",
+      "SHADOW_ALPHA",
+      "SHADOW_TOGGLED",
+      "TITLEBAR",
+      "EDITOR_LINES_INCREASED",
+      "EDITOR_LINES_TOGGLED",
+      "EDITOR_FONT_SIZE_INCREASED",
+      "EDITOR_FONT_CHANGED",
+      "BG_COLOR"
+    ].reduce((obj, v, i) => {
+      obj[v] = i + 1;
+      return obj;
+    }, {})
+  ),
+
   withEventChange(event_id, f) {
     return val => f(event_id, val);
   }
