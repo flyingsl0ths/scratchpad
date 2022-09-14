a3de06a77aedb4bf18394cb24caaec233db5ee7d
flyingsl0ths <randomemail32110@gmail.com>
2022-09-13 20:42:22 -0500

    Remove button

--- src/components/tabs/Theme.js
+++ src/components/tabs/Theme.js
@@ -1,10 +1,8 @@
 import React from "react";
-import IconButton from "@mui/material/IconButton";
-import AddIcon from "@mui/icons-material/Add";
 import BrushIcon from "@mui/icons-material/Brush";
 import PropTypes from "prop-types";
 
-import { VBox, HBox } from "../Containers";
+import { VBox } from "../Containers";
 import EditorThemes from "../../EditorThemes";
 import CodeWindowChanges from "../../CodeWindowEvents";
 import Spacer from "../Spacer";
@@ -20,14 +18,9 @@ export default function ThemeTab(props) {
   const { CODE_WINDOW_CHANGES } = CodeWindowChanges;
   return (
     <VBox id="theme-tab" className="tab-item" centered={false}>
-      <HBox className="sp-b" centered={false}>
-        <LabeledIcon label="Themes">
-          <BrushIcon />
-        </LabeledIcon>
-        <IconButton color="primary" aria-label="Add theme">
-          <AddIcon />
-        </IconButton>
-      </HBox>
+      <LabeledIcon label="Themes">
+        <BrushIcon />
+      </LabeledIcon>
       <Spacer amount="0.5em" />
       <ButtonList
         orientation="v"
